43faf14825b45ac983552c2ab0fdb985f4e78fcd
Paula Navarrete Díaz <paula@Paulas-MacBook-Air.local>
2023-08-17 17:31:21 -0400

    marginal contribution function

--- agent_utils.py
+++ agent_utils.py
@@ -15,6 +15,30 @@ class Agent:
                 T.pop(i)
         slots=[]
         for i in range(0,len(T)):
-            slots.append(bundle[i].timeslot)
+            slots.append(T[i].timeslot)
         slots=set(slots)
         return min(len(slots), self.cap)
+
+    def marginalContribution(self,bundle,item):
+        if item.item_id not in self.desired_classes:
+            print('if 1')
+            return 0
+        T=bundle.copy()
+        x=[*range(len(bundle))]
+        x.reverse()
+        for i in x:
+            g=bundle[i]
+            if g.item_id not in self.desired_classes:
+                T.pop(i)
+        slots=[]
+        for i in range(0,len(T)):
+            slot=T[i].timeslot
+            slots.append(slot)
+            if slot==item.timeslot:
+                print('if 2')
+                return 0
+        slots=set(slots)
+        if len(slots)>=self.cap:
+            print('if 3')
+            return 0
+        return 1
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from item_utils import generate_items_from_schedule
 
 
 items=generate_items_from_schedule('fall2023schedule.xlsx')
-agent=Agent('student1',[items[0].item_id,  items[20].item_id, items[25].item_id,items[30].item_id, items[40].item_id], 4)
-bundle=[items[0],items[20], items[25], items[30], items[40]]
+agent=Agent('student1',[items[0].item_id,  items[20].item_id, items[25].item_id,items[30].item_id,items[50].item_id, items[40].item_id], 4)
+bundle=[items[0],items[20], items[25], items[30], items[24], items[40]]
 print(agent.valuation(bundle))
+print(agent.marginalContribution(bundle, items[50]))
