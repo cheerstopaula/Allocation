6c0f9ec7b13e3569d121d965dc3111706988d5eb
Paula Navarrete Díaz <paula@Paulas-MacBook-Air.local>
2023-12-22 19:09:05 -0300

    update

--- src/allocation/metric_functions.py
+++ src/allocation/metric_functions.py
@@ -6,7 +6,7 @@ def utilitarian_welfare(X, agents,items):
     util = 0
     for agent_index in range(len(agents)):
         agent=agents[agent_index]
-        bundle=get_bundle_from_allocation_matrix(X,items,agent_index+1)
+        bundle=get_bundle_from_allocation_matrix(X,items,agent_index)
         val=agent.valuation(bundle)
         util += val
     return util/(len(agents)) #The number of agents is given by dim(X[(0)])-1, so as to not consider agent 0
@@ -17,7 +17,7 @@ def nash_welfare(X, agents,items):
     num_zeros = 0
     for agent_index in range(len(agents)):
         agent=agents[agent_index]
-        bundle=get_bundle_from_allocation_matrix(X,items,agent_index+1)
+        bundle=get_bundle_from_allocation_matrix(X,items,agent_index)
         val=agent.valuation(bundle)
         if (val== 0):
             num_zeros+= 1
